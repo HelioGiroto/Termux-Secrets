53408a96316f6e18533160e528d0145b898b6045
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-11-24 01:04:21 -0200

    Update youtube-dl4termux.sh

--- youtube-dl4termux.sh
+++ youtube-dl4termux.sh
@@ -5,4 +5,5 @@ pkg install python -y
 pkg install ffmpeg -y
 curl -s -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
 chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
+termux-setup-storage
 # youtube-dl -U		# Para atualizar se caso necessite posteriormente
