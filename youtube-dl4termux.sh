baa0dff878e2394e1423728813be50f6b2da78b0
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-11-24 01:09:15 -0200

    Delete youtube-dl4termux.sh

--- youtube-dl4termux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkg up
-pkg install curl -y
-pkg install python -y
-pkg install ffmpeg -y
-curl -s -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
-chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
-termux-setup-storage
-# youtube-dl -U		# Para atualizar se caso necessite posteriormente
